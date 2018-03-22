6960923ac63372f444b1b34c58fc95aec3e0ad60
leandro <leandrocgms1986#outlook.com>
2018-03-22 11:45:45 -0400

    Navegando entre Activities.

--- app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
+++ app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
@@ -1,14 +1,56 @@
 package atmempresaconsultoria.cursoandroid.com.atmempresaconsultoria;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private ImageView botaoEmpresa;
+    private ImageView botaoServicos;
+    private ImageView botaoClientes;
+    private ImageView botaoContatos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoEmpresa = findViewById(R.id.empresaId);
+        botaoServicos = findViewById(R.id.servicoId);
+        botaoClientes = findViewById(R.id.clienteId);
+        botaoContatos = findViewById(R.id.contatoId);
+
+        botaoEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
+
+        botaoServicos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ServicoActivity.class));
+            }
+        });
+
+        botaoClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        botaoContatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
